0ee108321f10441c2515aeb707ff1b1b0c5d8de7
wxrighttoleft <zhuiqiuxinf@163.com>
2015-03-24 23:43:52 +0800

    wx-first commit

---
+++ src/com/whx/ui/StartIndexFrame.java
@@ -0,0 +1,13 @@
+package com.whx.ui;
+
+import java.awt.Frame;
+
+public class StartIndexFrame extends Frame {
+
+	private static final long serialVersionUID = 3550972842695616566L;
+
+	public StartIndexFrame(String str){
+		super(str);
+	}
+
+}
